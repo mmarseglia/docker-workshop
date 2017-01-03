010d6e1006d461fb2992c4832c23eb712da55235
Mike Marseglia <mike.marseglia@hasbro.com>
2017-01-03 11:39:02 -0500

    initial commit

---
+++ install_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+sudo yum -y install docker-engine
+sudo systemctl enable docker.service
+sudo systemctl start docker
+
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
+sudo chmod +x /usr/local/bin/docker-machine
